aa781804717744f0b091fc1cdd9c63b502b8d3c2
abinash1011 <90611005+abinash1011@users.noreply.github.com>
2021-09-15 21:34:37 +0530

    Add files via upload

---
+++ input_many-one.py
@@ -0,0 +1,2 @@
+ch = input("Enter a number or charachter: ")[0]
+print(ch)
---
+++ input_power.py
@@ -0,0 +1,3 @@
+num1 = (int(input("Enter a number to get its power: ")))
+num2 = (int(input("Now enter a its power: ")))
+print(num1**num2)
